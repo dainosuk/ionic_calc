28cdf751e71af1f54be2d174eea71e3224c2f0d3
dainosuk <dainosuk@gmail.com>
2020-01-11 15:09:56 +0900

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  initValue: number;  // 元本入力欄と双方向データバインド
+  rate: number;       // 金利入力欄と双方向データバインド
+
+  constructor() {}
+
+  // 複利計算
+  calc(): number {
+    if (isNaN(this.initValue) || isNaN(this.rate)) {
+      // 元本または利率が数字でないときはnullを返す
+      return null;
+    }
+    let answer: number = this.initValue;
+    for (let i = 0; i < 10; i++) {
+      // 金利計算を10回繰り返して複利計算
+      answer = answer * (1 + this.rate / 100)
+    }
+    return Math.floor(answer);  // 計算結果を整数に変換
+  }
+
+  // 年毎の金額計算
+  calcArray(): number[] {
+    if (isNaN(this.initValue) || isNaN(this.rate)) {
+    // 元本または利率が数字でないときはnullを返す
+    return null;
+    }
+    let answer: number = this.initValue;
+    let ret: number[] = [answer];
+    for (let i = 0; i < 10; i++) {
+      // 金利計算を10回繰り返して複利計算
+      answer = answer * (1 + this.rate / 100)
+      ret.push(Math.floor(answer)); // 整数に変換
+    }
+    return ret;
+}
+
+}
+
+
